adc2d5df4d274a744911cd11621eeebae5fe7516
Kett, Oliver <oliver.kett@fau.de>
2014-09-30 13:19:29 +0200

    Initial Commit

---
+++ SigfoodTests/SigfoodTests.swift
@@ -0,0 +1,36 @@
+//
+//  SigfoodTests.swift
+//  SigfoodTests
+//
+//  Created by Kett, Oliver on 30.09.14.
+//  Copyright (c) 2014 Kett, Oliver. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SigfoodTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
