97062a9daa23ecc3376c6f4edd7a17e6f6762496
Kett, Oliver <oliver.kett@fau.de>
2014-09-30 13:19:29 +0200

    Initial Commit

---
+++ Sigfood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sigfood
+//
+//  Created by Kett, Oliver on 30.09.14.
+//  Copyright (c) 2014 Kett, Oliver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
