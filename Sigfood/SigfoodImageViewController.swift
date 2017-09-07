cb5d5cbb0a8bd046774c31d7dafb0b9d1f1ca4b0
Oliver Kett <oliver.kett@fau.de>
2017-09-07 10:39:46 +0200

    SigfoodImageViewController also uses the new loading animation

--- Sigfood/SigfoodImageViewController.swift
+++ Sigfood/SigfoodImageViewController.swift
@@ -7,16 +7,20 @@
 //
 
 import UIKit
+import JGProgressHUD
 
 class SigfoodImageViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     
     var imageID: Int = 0
+    // initialize loading animation bevore first use
+    var loadingAnimation = JGProgressHUD(style: JGProgressHUDStyle.Dark)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        SwiftSpinner.show("loading")
+        // SwiftSpinner.show()
+        loadingAnimation.showInView(self.view)
         NSOperationQueue().addOperationWithBlock {
             guard let data = NSData(contentsOfURL: NSURL(string: "https://www.sigfood.de/?do=getimage&bildid=\(self.imageID)&width=800")!) else { self.hideView(nil); return }
             guard let image = UIImage(data: data) else { self.hideView(nil); return }
@@ -24,13 +28,15 @@ class SigfoodImageViewController: UIViewController, UIGestureRecognizerDelegate 
                 self.view.backgroundColor = UIColor(red: 0.016, green: 0.710, blue: 0.788, alpha: 1.00)
                 self.imageView.contentMode = .ScaleAspectFit
                 self.imageView.image = image
-                SwiftSpinner.hide()
+                // SwiftSpinner.hide()
+                self.loadingAnimation.dismissAnimated(false)
             }
         }
     }
     
     @IBAction func hideView(sender: AnyObject?) {
-        SwiftSpinner.hide()
+        // SwiftSpinner.hide()
+        self.loadingAnimation.dismissAnimated(false)
         presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
 
